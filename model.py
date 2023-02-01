00615b0372708100cf4c2e47b4c5fff6d38c67b3
sruthisrini <sruthisrinivasan@outlook.in>
2023-02-01 06:11:33 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,111 @@
+import torch
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pack_padded_sequence
+import torch.nn as nn
+import torch.nn.functional as F
+
+class RNNDataset(Dataset):
+    def __init__(self, in_data, in_labels):
+        self.data = in_data
+        self.labels = in_labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, n_class, device,
+                 lstm_n_layers=2,
+                 lstm_hidden_dim=32,
+                 bidirect=True,
+                 add_feat=False,
+                 dropout_rate=0.5,
+                 add_fc_layer=True,
+                 embed=True,
+                 embed_vocab_size=5,
+                 embed_dim=10):
+
+        super(LSTMModel, self).__init__()
+
+        self.bidirect = bidirect
+        self.embed = embed
+        self.add_feat = add_feat
+        self.lstm_n_layers = lstm_n_layers
+        self.lstm_hidden_dim = lstm_hidden_dim
+        self.device = device
+        self.add_fc_layer = add_fc_layer
+        self.embedding = nn.Embedding(embed_vocab_size, embed_dim)
+        # Dropout.
+        self.dropout = nn.Dropout(dropout_rate)
+        # LSTM.
+        if embed:
+            if add_feat:
+                self.lstm = nn.LSTM(embed_dim + (input_dim - 1), lstm_hidden_dim, lstm_n_layers,
+                                   bidirectional=bidirect, bias=True,
+                                   batch_first=True).to(device)
+            else:
+                self.lstm = nn.LSTM(embed_dim, lstm_hidden_dim, lstm_n_layers,
+                                   bidirectional=bidirect, bias=True,
+                                   batch_first=True).to(device)
+        else:
+            self.lstm = nn.LSTM(input_dim, lstm_hidden_dim, lstm_n_layers,
+                               bidirectional=bidirect, bias=True,
+                               batch_first=True).to(device)
+        if bidirect:
+            if add_fc_layer:
+                self.fc1 = nn.Linear(2*lstm_hidden_dim, lstm_hidden_dim)
+                self.fc2 = nn.Linear(lstm_hidden_dim, n_class)
+            else:
+                self.fc = nn.Linear(2*lstm_hidden_dim, n_class)
+        else:
+            if add_fc_layer:
+                self.fc1 = nn.Linear(lstm_hidden_dim, int(lstm_hidden_dim/2))
+                self.fc2 = nn.Linear(int(lstm_hidden_dim/2), n_class)
+            else:
+                self.fc = nn.Linear(lstm_hidden_dim, n_class)
+
+
+    def zero_state(self, batch_size):
+        if self.bidirect:
+            h0 = torch.zeros(2*self.lstm_n_layers, batch_size, self.lstm_hidden_dim, dtype=torch.float).requires_grad_().to(self.device)
+            c0 = torch.zeros(2*self.lstm_n_layers, batch_size, self.lstm_hidden_dim, dtype=torch.float).requires_grad_().to(self.device)
+        else:
+            h0 = torch.zeros(self.lstm_n_layers, batch_size, self.lstm_hidden_dim, dtype=torch.float).requires_grad_().to(self.device)
+            c0 = torch.zeros(self.lstm_n_layers, batch_size, self.lstm_hidden_dim, dtype=torch.float).requires_grad_().to(self.device)
+        return h0, c0
+
+    def forward(self, batch_data, batch_lens, batch_size):
+        h0, c0 = self.zero_state(batch_size)
+
+        if self.embed:
+            x_embed = self.embedding(batch_data[:, :, 0].long()).clone().detach().requires_grad_(True)
+            if self.add_feat:
+                x_embed = torch.cat([x_embed, batch_data[:, :, 1:]], dim=2)
+        else:
+            x_embed = batch_data.clone().detach().requires_grad_(True)
+
+        x_packed = pack_padded_sequence(x_embed, batch_lens, batch_first=True, enforce_sorted=False)
+
+        if self.bidirect:
+            out, (hidden, cell) = self.lstm(x_packed, (h0, c0))
+            hidden = torch.cat([hidden[0], hidden[1]], dim=1).unsqueeze(0)
+        else:
+            out, (hidden, cell) = self.lstm(x_packed, (h0, c0))
+
+        x = self.dropout(hidden)
+
+        if self.add_fc_layer:
+            x = self.dropout(self.fc1(x))
+            x = self.fc2(x)
+        else:
+            x = self.fc(x)
+
+        return x, x_embed
+
+
+if __name__ == "__main__":
+    
+    test_model = LSTMModel(input_dim=3, n_class=30, device="cpu")
+     
---
+++ transform.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import DataLoader,Dataset
+import math
+from model import RNNDataset
+from torch.nn.utils.rnn import pad_sequence
+
+
+def read_data_to_list(csv_file, skip_n_seqs=True):
+    RNA_data = pd.read_csv(csv_file, header=0)
+    RNA_seq = RNA_data["Sequence"].tolist()
+
+    RNA_labels = RNA_data.iloc[::, 1:]
+    RNA_labels = RNA_labels.values.tolist()
+
+    return RNA_seq, RNA_labels
+
+def string_vectorizer(seq,empty_vectors=False,embed_numbers=False,embed_one_vec=False,custom_alphabet=False):
+    alphabet=['A','C','G','T']
+    if custom_alphabet:
+        alphabet = custom_alphabet
+    if empty_vectors:
+        vector = []
+        for letter in seq:
+            vector.append([])
+    else:
+        if embed_numbers:
+            vector = []
+            ab2nr_dic = {}
+            for idx,c in enumerate(alphabet):
+                ab2nr_dic[c] = idx+1
+            for letter in seq:
+                idx = ab2nr_dic[letter]
+                if embed_one_vec:
+                    vector.append(idx)
+                else:
+                    vector.append([idx])
+        else:
+            vector = torch.Tensor([[1 if char == letter else 0 for char in alphabet] for letter in seq])
+    return vector
+
+
+def prepare_data(csv_filename):
+    rna_seq, rna_labels = read_data_to_list(csv_filename)
+    rna_vec = []
+    for seq in rna_seq:
+        vec = string_vectorizer(seq)
+        rna_vec.append(vec)
+
+    return rna_vec, rna_labels
+
+def pad_collate(batch):
+    (xs, ys) = zip(*batch)
+    xs_lens = [len(x) for x in xs]
+    xs_pad = pad_sequence(xs, batch_first=True, padding_value=0)
+    ys = torch.FloatTensor([[y] for y in ys])
+    return xs_pad, ys 
+
+
+if __name__ == "__main__":
+    csv_file_path = r"D:\University_of_Freiburg\Semester-4\project_2\K562\K562_sequenced_data.csv"
+    rna_vecs,rna_labels = prepare_data(csv_file_path)
+    data=RNNDataset(rna_vecs,rna_labels)
+    dataloader=DataLoader(dataset=data,batch_size=32,shuffle=True,collate_fn=pad_collate,pin_memory=True)
+
+    dataiter=iter(dataloader)
+    datas=dataiter.next()
+    features,*labels=datas
+    print("features:",features)
+    print("labels:",labels)
