83c6dbf67f27cb512b65852f12b3022e0f4a006f
sruthisrini <sruthisrinivasan@outlook.in>
2023-03-19 06:52:11 +0100

    Update binary_classifier.py

--- binary_classifier.py
+++ binary_classifier.py
@@ -47,7 +47,7 @@ def train(model, optimizer, train_loader, criterion, batch_size, device):
             f1_acc+=f1_acc
 
         except:
-            outputs = outputs.reshape([2, 1,1])
+            outputs = outputs.reshape([25, 1,1])
             loss = criterion(outputs, batch_labels)
             loss_all_train += loss.item() * len(batch_labels)
             loss.backward()
@@ -55,8 +55,8 @@ def train(model, optimizer, train_loader, criterion, batch_size, device):
             sigmoid_outputs=torch.sigmoid(outputs)
             sigmoid_outputs[np.where(sigmoid_outputs>=0.5)]=1
             sigmoid_outputs[np.where(sigmoid_outputs<0.5)]=0
-            sigmoid_outputs=sigmoid_outputs.reshape([2,1])
-            batch_labels=batch_labels.reshape([2,1])
+            sigmoid_outputs=sigmoid_outputs.reshape([25,1])
+            batch_labels=batch_labels.reshape([25,1])
             f1_acc=f1_score(sigmoid_outputs.detach().numpy().astype(int), batch_labels.detach().numpy().astype(int), average='weighted')
             f1_acc+=f1_acc
 
@@ -90,7 +90,7 @@ def validation(test_loader, model, batch_size, criterion, device):
             f1_acc+=f1_acc
 
         except:
-            outputs = outputs.reshape([17, 1,1])
+            outputs = outputs.reshape([31, 1,1])
             loss = criterion(outputs, batch_labels)
             loss_all_validation += loss.item() * len(batch_labels)
             loss.backward()
@@ -98,8 +98,8 @@ def validation(test_loader, model, batch_size, criterion, device):
             sigmoid_outputs=torch.sigmoid(outputs)
             sigmoid_outputs[np.where(sigmoid_outputs>=0.5)]=1
             sigmoid_outputs[np.where(sigmoid_outputs<0.5)]=0
-            sigmoid_outputs=sigmoid_outputs.reshape([17,1])
-            batch_labels=batch_labels.reshape([17,1])
+            sigmoid_outputs=sigmoid_outputs.reshape([31,1])
+            batch_labels=batch_labels.reshape([31,1])
             f1_acc=f1_score(sigmoid_outputs.detach().numpy().astype(int), batch_labels.detach().numpy().astype(int), average='weighted')
             f1_acc+=f1_acc
         
