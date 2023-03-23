035fe4e38bc9818013f440f421623cec14914c26
sruthisrini <sruthisrinivasan@outlook.in>
2023-03-23 17:54:56 +0100

    Update binary_classifier.py

--- binary_classifier.py
+++ binary_classifier.py
@@ -93,7 +93,7 @@ def test(test_loader, model, criterion, device):
     for batch_data, batch_labels, batch_lens in test_loader:
         outputs, _ = model(batch_data, batch_lens, len(batch_labels))
         outputs = outputs.reshape([len(batch_lens), 1])
-        batch_labels=batch_labels.reshape([batch_size, 1])
+        batch_labels=batch_labels.reshape([len(batch_lens), 1])
         acc = binary_accuracy(outputs, batch_labels)
         test_acc += acc.item()
         sigmoid_outputs=torch.sigmoid(outputs)
